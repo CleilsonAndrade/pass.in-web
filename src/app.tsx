8399c5011226a7e670e1ba3ee0941bb7e91fd364
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-08-21 19:19:48 -0300

    ✨ feat: semi-header with input

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import { Header } from "./components/header";
 export function App() {
 
   return (
-    <div className="max-w-[1216px] mx-auto py-5">
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
       <Header />
       <AttendeeList />
     </div>
--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,5 +1,17 @@
+import { Search } from "lucide-react";
+
 export function AttendeeList() {
   return (
-    <div>AttendeeList</div>
+    <div>
+      <div className="flex gap-3 items-center">
+        <h1 className="text-2xl">Participantes</h1>
+        <div className="w-72 px-3 py-1.5 border border-white/10 bg-transparent rounded-lg text-sm flex items-center gap-3">
+          <Search className="size-4 text-emerald-300" />
+          <input placeholder="Buscar participantes..." className="bg-transparent flex-1 outline-none" />
+        </div>
+      </div>
+
+
+    </div>
   )
 }
